2c63f9f31c98203dfe1a8a1286c6b4f2a0050f4d
Adányi Barnabás <adanyi@gmail.com>
2021-04-29 11:31:22 +0200

    Tested, completed.

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try (FileWriter fw = new FileWriter(Paths.get(output).toFile())) {
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                fw.append(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        }
+    }
+
+}
